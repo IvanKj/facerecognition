aabf5efef16297ef01352050c0759a7b6bc88acc
IvanKj <37956419+IvanKj@users.noreply.github.com>
2018-04-03 01:35:20 +0200

    uppl

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+const register = require('./controllers/Register');
+const signin = require('./controllers/SignIn');
+const profile = require('./controllers/Profile');
+const image = require('./controllers/Image');
+
+
+const db = knex({
+	client: 'pg',
+	connection: {
+		host: '127.0.0.1',
+		user: 'postgres',
+		password: '2smart4U',
+		database: 'SmartBrain'
+	}
+})
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+app.get('/', (req, res)=> { res.send(database.users) })
+app.post('/signin', signin.handleSignin(db, bcrypt))
+app.post('/register', register.handleRegister(db, bcrypt))
+app.get('/profile/:id', profile.handleProfile(db))
+app.put('/image', (req, res) => { image.handleImage(req, res, db) })
+app.post('/imageurl', (req, res) => { image.handleImage(req,res, db) })
+app.listen(3000, ()=> {
+	console.log('app is running on port 3000');
+})
